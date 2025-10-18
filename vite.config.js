7f1c6091948b803b6e84155d9f2a2006da729602
emiryavuz1 <164385758+emiryavuz1@users.noreply.github.com>
2025-10-18 15:47:08 +0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "child2",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./src/App",
+      },
+      shared: {
+        react: { singleton: true, requiredVersion: "^19.1.1" },
+        "react-dom": { singleton: true, requiredVersion: "^19.1.1" },
+        "@mantine/core": { singleton: true, requiredVersion: "^8.3.5" },
+        "@mantine/hooks": { singleton: true, requiredVersion: "^8.3.5" },
+      },
+    }),
+  ],
+  resolve: {
+    dedupe: ["react", "react-dom", "@mantine/core", "@mantine/hooks"],
+  },
+  server: {
+    port: 3002,
+  },
+});
